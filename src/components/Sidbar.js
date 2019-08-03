eb2387aff5199be37ac733f8cfbafd7cc5932873
haveeru <suood_mohamed@hotmail.com>
2019-08-03 20:11:25 +0500

    linkdata

--- src/components/Sidbar.js
+++ src/components/Sidbar.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import {ProductConsumer} from '../context';
+
 
 const Sidbar = () => {
     return (
-        <>
-            hello from Sidbar!
-        </>
+        <ProductConsumer>
+            {value => { 
+                const {links, sidebarOpen, handleSidebar} = value; 
+
+                return <SideWrapper>
+                    <ul>
+                        {links.map(link => {
+                            return (
+                                <li key={link.id}>
+                                    <Link to={link.path} className='siebar-link'>{link.text}</Link>
+                                </li>
+                            )
+                        })} 
+                    </ul>
+                </SideWrapper>
+            }}
+        </ProductConsumer>
     );
 };
 
 export default Sidbar;
+
+const SideWrapper = styled.nav`
+
+`;
