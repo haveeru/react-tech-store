422900a49fbe09c2611386f33c7587ff2ef1711d
haveeru <suood_mohamed@hotmail.com>
2019-08-03 11:42:29 -0700

    side bar

--- src/components/Sidbar.js
+++ src/components/Sidbar.js
@@ -10,12 +10,12 @@ const Sidbar = () => {
             {value => { 
                 const {links, sidebarOpen, handleSidebar} = value; 
 
-                return <SideWrapper>
+                return <SideWrapper show = {sidebarOpen}>
                     <ul>
                         {links.map(link => {
                             return (
                                 <li key={link.id}>
-                                    <Link to={link.path} className='siebar-link'>{link.text}</Link>
+                                    <Link to={link.path} className='sidebar-link'>{link.text}</Link>
                                 </li>
                             )
                         })} 
@@ -29,5 +29,36 @@ const Sidbar = () => {
 export default Sidbar;
 
 const SideWrapper = styled.nav`
-
+  position: fixed;
+  top: 61px;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: var(--mainGrey);
+  z-index: 1;
+  border-right: 4px solid var(--primaryColor);
+  transition: var(--mainTransition);
+  transform: ${props => (props.show ? "translateX(0)" : "translateX(-100%)")};
+  ul {
+    list-style-type: none;
+    padding: 0 !important;
+  }
+  .sidebar-link {
+    display: block;
+    font-size: 1.5rem;
+    text-transform: capitalize;
+    color: var(--mainBlack);
+    padding: 0.5rem 1.5rem;
+    background: transparent;
+    transition: var(--mainTransition);
+  }
+  .sidebar-link:hover {
+    background: var(--primaryColor);
+    color: var(--mainWhite);
+    padding: 0.5rem 1.5rem 0.5rem 2.5rem;
+    text-decoration: none;
+  }
+  @media (min-width: 576px) {
+    width: 20rem;
+  }
 `;
