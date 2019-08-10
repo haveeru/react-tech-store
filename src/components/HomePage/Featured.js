d899873f7a18072ef266f217c7cdc2de25ac0233
haveeru <suood_mohamed@hotmail.com>
2019-08-10 13:01:45 -0700

    featured products

--- src/components/HomePage/Featured.js
+++ src/components/HomePage/Featured.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Product from '../Product';
+import Title from '../Title';
+import {Link} from 'react-router-dom';
+import {ProductConsumer} from '../../context';
 
 const Featured = () => {
     return (
-        <div>
-            Hello from Featured.
-        </div>
+        <section className="py-5">
+           <div className="container">
+                <Title title="featured products" center="true" />
+           </div>
+           <div className="row">
+                <ProductConsumer>
+                    {value=>{
+                        const {featuredProducts} = value;
+                        return featuredProducts.map(product => (
+                            <Product key={product.id} product={product}></Product>
+                        ));
+                    }}
+                </ProductConsumer>
+           </div>
+        </section>
     );
 };
 
---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Product = () => {
+    return (
+        <div>
+            product component
+        </div>
+    );
+};
+
+export default Product;
