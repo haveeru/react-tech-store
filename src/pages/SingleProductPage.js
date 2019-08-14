f3084d8f55b4a202f258ac9318491b5ef87ed86e
haveeru <suood_mohamed@hotmail.com>
2019-08-14 01:50:28 -0700

    set single product page

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import Hero from '../components/Hero';
+import singleProductImg from '../images/singleProductBcg.jpeg';
+import {ProductConsumer} from '../context';
 
 const SingleProductPage = () => {
     return (
         <>
-            <h1>Hellow from Single Products Page</h1>
+            <Hero img={singleProductImg} title='singe product' />
+            <ProductConsumer>
+                {value=>{
+                    const {singleProduct, addToCart, loading} = value;
+
+                    if(loading){
+                        console.log('hell from loading');
+                        return <h1>product loading</h1>
+                    }
+                    const {company, description, id, price, title, image} = singleProduct;
+                    return <section className="py-5">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-10 mx-auto co-sm-8 col-md-6 my-3">
+                                    <img 
+                                        src={`../${image}`} 
+                                        //src = {image}
+                                        alt="single product" 
+                                        className="img-fluid"/>
+                                </div>
+                                <div className="col-10 mx-auto co-sm-8 col-md-6 my-3">
+                                    <h5 className="text-title mb-4">model : {title}</h5>
+                                    <h5 className="text-capitalize text-muted mb-4">company : {company}</h5>
+                                    <h5 className="text-main text-capitalize mb-4">price : ${price}</h5>
+                                    <p className="text-capitalize text-title mt-3">
+                                        some info about product :
+                                    </p>
+                                    <p>{description}</p>
+                                    <button title="button" className="main-link"
+                                        style={{margin: '0.75rem'}}  
+                                        onClick={()=> addToCart(id)}
+                                    >
+                                        add to cart
+                                    </button>
+                                    <Link to='/products' className="main-link" 
+                                        style={{margin: '0.75rem'}}
+                                    >
+                                    back to products
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </section>;
+                }}
+            </ProductConsumer>
         </>
     );
 };
