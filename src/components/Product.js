1a3749b6b55c6aaac8b7b8ddd94f16da02ad8497
haveeru <suood_mohamed@hotmail.com>
2019-08-15 12:48:54 -0700

    product update

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import styled from 'styled-components';
-import {Link} from 'react-router-dom';
-import {FaSearch, FaCartPlus} from 'react-icons/fa';
-import {ProductConsumer} from '../context';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { FaSearch, FaCartPlus } from "react-icons/fa";
+import { ProductConsumer } from "../context";
 
-const Product = ({product}) => {
-    return (
-        <ProductConsumer>
-             {value => {
+export default function Product({ product }) {
+  return (
+    <ProductConsumer>
+      {value => {
         const { addToCart, setSingleProduct } = value;
+
         return (
           <ProductWrapper className="col-10 mx-auto col-sm-8 col-md-6  col-lg-4 my-3">
             <div className="card">
@@ -40,54 +41,52 @@ const Product = ({product}) => {
           </ProductWrapper>
         );
       }}
-        </ProductConsumer>
-    );
-};
-
-export default Product;
+    </ProductConsumer>
+  );
+}
 
 const ProductWrapper = styled.div`
-    .card {
-        box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.3);
-        transition: var(--mainTransition);
-        height: 100%;
-    }
-    .card:hover {
-        box-shadow: 7px 10px 5px 0px rgba(0, 0, 0, 0.5);
-        height: 100%;
-        cursor: pointer;
-    }
-    .card-img-top {
-        transition: var(--mainTransition);
-    }
-    .card:hover .card-img-top{
-        transform: scale(1.15);
-        opacity: 0.2;
-    }
-    .img-container {
-        positioin: relative;
-    }
-    .product-icons {
-        transition: var(--mainTransition);
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, 50%);
-    }
-    .icon {
-        font-size: 2.5rem;
-        margin: 1rem;
-        padding: 0.5rem;
-        color: var(--primaryColor);
-        background: var(--mainBlack);
-        border-radius: 0.5rem;
-    }
-    .card:hover .product-icons {
-        opacity: 1;
-    }
-    .card-body {
-        font-weight: bold;
-        letter-spacing: 2px;
-        text-transform: uppercase;
-    }
+  .card {
+    box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.3);
+    transition: var(--mainTransition);
+    height: 100%;
+  }
+  .card:hover {
+    box-shadow: 7px 10px 5px 0px rgba(0, 0, 0, 0.5);
+    cursor: pointer;
+  }
+  .card-img-top {
+    transition: var(--mainTransition);
+  }
+  .card:hover .card-img-top {
+    transform: scale(1.15);
+    opacity: 0.2;
+  }
+  .img-container {
+    position: relative;
+  }
+  .product-icons {
+    transition: var(--mainTransition);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    opacity: 0;
+  }
+  .icon {
+    font-size: 2.5rem;
+    margin: 1rem;
+    padding: 0.5rem;
+    color: var(--primaryColor);
+    background: var(--mainBlack);
+    border-radius: 0.5rem;
+  }
+  .card:hover .product-icons {
+    opacity: 1;
+  }
+  .card-body {
+    font-weight: bold;
+    letter-spacing: 2px;
+    text-transform: uppercase;
+  }
 `;
