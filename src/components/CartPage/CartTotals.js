143ddc88a5569a7e3ad413c6c6ed2d2fc1b160f3
haveeru <suood_mohamed@hotmail.com>
2019-08-15 13:50:12 +0500

    cart total component

--- src/components/CartPage/CartTotals.js
+++ src/components/CartPage/CartTotals.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {ProductConsumer} from '../../context';
 
 const CartTotals = () => {
     return (
-        <div>
-            CartTotals
+        <div className="container">
+            <div className="row">
+                <ProductConsumer>
+                    {value=>{
+                        const {clearCart, cartSubTotal, cartTax, cartTotal} = value;
+                        return <div className="col text-title text-center my-4">
+                            <button className="btn btn-outline-danger text-capitalize mb-4">
+                                clear cart
+                            </button>
+                            <h3>subtotal :  ${cartSubTotal}</h3>
+                            <h3>tax :  ${cartTax}</h3>
+                            <h3>total :  ${cartTotal}</h3>
+                        </div>
+                    }}
+                </ProductConsumer>
+            </div>
         </div>
     );
 };
