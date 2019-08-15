c3b23ab0e4043a902c7023b3b1d885d5bdac15dc
haveeru <suood_mohamed@hotmail.com>
2019-08-15 18:12:41 +0500

    filter products component

---
+++ src/components/ProductsPage/ProductFilter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FilterProducts extends Component {
+    render() {
+        return (
+            <div>
+                Filtering Component
+            </div>
+        );
+    }
+}
+
+export default FilterProducts;
