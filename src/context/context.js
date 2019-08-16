9fa9a6a240a8eda4816ede1a7cdba39241c7796c
haveeru <suood_mohamed@hotmail.com>
2019-08-16 20:26:40 +0500

    contentful setup

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { linkData } from "./linkData";
 import { socialData } from "./socialData";
-import { items } from "./productData";
+// import { items } from "./productData";
+import {client} from "./contentful";
+
 const ProductContext = React.createContext();
 //Provider
 //Consumer
@@ -31,7 +33,12 @@ class ProductProvider extends Component {
   componentDidMount() {
     //from contentful items
 
-    this.setProducts(items);
+    //this.setProducts(items);
+    client.getEntries({
+      content_type: 'techStoreProducts'
+    })
+      .then((response) => this.setProducts(response.items))
+      .catch(console.error)
   }
 
   //set products
